1ec715096cd6d184de7ea8271348eb52e402e436
Vladimir Popov <vladimir.popov@imagination.com>
2013-07-31 19:36:33 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var _ = require('underscore');
+
+var Model = function(attrs) {
+  
+  this.attributes = attrs;
+  
+  _.each(this.attributes, function(v, k) {
+    this.__defineGetter__(k, function(){
+      return v;
+    });
+    
+    this.__defineSetter__(k, function(val){
+      this.attributes[k] = val;
+      // do your custom stuff (propagate events)
+    });
+  })
+}
